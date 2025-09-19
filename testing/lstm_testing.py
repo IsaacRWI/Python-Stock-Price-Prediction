cf9fc237bfe90b8e98827946c51d60af4f8e6e1c
Isaac <isaacngrwi@gmail.com>
2025-09-19 20:55:38 +0100

    Added function to change df index date from string to datetime - Then realised that datatype for yfinance date is already datetime

--- testing/lstm_testing.py
+++ testing/lstm_testing.py
@@ -1,6 +1,22 @@
+import pandas as pd
 import yfinance as yf
 import numpy as np
+import pandas
+import datetime
 
 df = yf.download("^GSPC", start="2010-01-01")
+# print(df)
 
+df = df[["Close"]]  # keeps only the closing price column
 print(df)
+print(df.index)
+
+"""
+def string_to_datetime (s):
+    split = s.split("-")
+    year, month, day = int(split[0]), int(split[1]), int(split[2])
+    return datetime.datetime(year = year, month = month, day = day)
+    
+
+df.index = df.index.apply(string_to_datetime)
+"""
